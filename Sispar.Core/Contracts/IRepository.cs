629f3f377df56ed24e79a809bc54deaa817519c5
flpsno <flpsno@hotmail.com>
2017-11-05 12:46:36 -0200

    Adicionada interface repository

---
+++ Sispar.Core/Contracts/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Sispar.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sispar.Core.Contracts
+{
+    public interface IRepository<T> : IDisposable where T : Entity
+    {
+        IEnumerable<T> Obter();
+        T ObterById(int id);
+        T Adicionar(T obj);
+        T Editar(T obj);
+        void Delete(T obj);
+    }
+}
