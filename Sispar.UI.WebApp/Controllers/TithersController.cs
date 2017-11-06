eb9ab1e6857635259f3ee945d1d71a4ddd741efe
Felipe Santos <flpsno@hotmail.com>
2017-11-06 13:13:24 -0200

    Adicionada listagem de dizimistas; Adicionada classe TitherRepository; Adicionada controller Tithers.

---
+++ Sispar.UI.WebApp/Controllers/TithersController.cs
@@ -0,0 +1,26 @@
+﻿using Sispar.Core.EF.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sispar.UI.WebApp.Controllers
+{
+    [Authorize]
+    public class TithersController : Controller
+    {
+        private readonly TitherRepository _ctx = new TitherRepository();
+
+        public ActionResult Index()
+        {
+            var tithers = _ctx.GetAll().OrderBy(t => t.Name);
+            return View(tithers);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _ctx.Dispose();
+        }
+    }
+}
