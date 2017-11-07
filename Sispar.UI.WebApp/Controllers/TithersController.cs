28307196538f3b1d7571388b6b72ac3bff7c3579
Felipe Santos <flpsno@hotmail.com>
2017-11-07 13:22:50 -0200

    Adicionada função para adicionar e excluir tither.

--- Sispar.UI.WebApp/Controllers/TithersController.cs
+++ Sispar.UI.WebApp/Controllers/TithersController.cs
@@ -1,4 +1,5 @@
 ﻿using Sispar.Core.EF.Repositories;
+using Sispar.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,42 @@ namespace Sispar.UI.WebApp.Controllers
         {
             var tithers = _ctx.GetAll().OrderBy(t => t.Name);
             return View(tithers);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Tither tither)
+        {
+            tither.NameSpouse = "AAAA";
+            tither.MarriegeDate = DateTime.Now;
+            tither.MatiralStatus = "2";
+
+            _ctx.Add(tither);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var tither = _ctx.GetById(id);
+
+            if (tither == null)
+                return HttpNotFound();
+
+            return View(tither);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Tither tAux)
+        {
+            var tither = _ctx.GetById(tAux.Id);
+            _ctx.Delete(tither);
+
+            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
